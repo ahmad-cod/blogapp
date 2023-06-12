03b95047f43c8922b3116035c823d9859be24056
ahmad-cod <ahmadadewale800@gmail.com>
2023-06-12 18:16:11 +0100

    Fix the referenceError of req

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,13 +2,8 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-    try {
-        const blogs = await Blog.find({})
-        response.json(blogs)
-    } catch (error) {
-        console.error(error)
-        response.status(500).send('Server Error')
-    }
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
 blogsRouter.get('/:id', async (request, response) => {
@@ -17,33 +12,30 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(req.body)
-    try {
-        const result = await blog.save()
-        response.json(result)
-    } catch (error) {
-        console.error(error)
-    }
+    const blog = new Blog({
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes || 0
+    })
+    const result = await blog.save()
+    console.log(result)
+    response.status(201).json(result)
 })
 
 blogsRouter.put('/:id', async (request, response) => {
+    const blog = await Blog.findByIdAndUpdate(request.params.id, req.body)
 
-    try {
-        const blog = await Blog.findByIdAndUpdate(request.params.id, req.body)
-        response.json(blog)  
-    } catch (error) {
-        console.error(error)   
+    if(blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
     }
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-
-    try {
-        const blog = await Blog.findByIdAndRemove(request.params.id)
-        response.status(204).end() 
-    } catch (error) {
-        console.error(error)
-    }
+    const blog = await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = blogsRouter
