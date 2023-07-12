a262ce8b1122c07646ddeda107a4fde1fdf1bdfa
ahmad-cod <ahmadadewale800@gmail.com>
2023-07-12 02:32:59 +0100

    ui build

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-app.use(requestLogger)
-
 app.use(express.static('dist'))
+
+app.use(requestLogger)
 
 app.use('/api/blogs', extractUser, blogsRouter)
 app.use('/api/users', usersRouter)
